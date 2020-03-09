9b5a47493f1994d3e61631b0f0ce4ff059727648
Asgar Zagitov <thorinhood@yandex-team.ru>
2020-03-09 18:29:05 +0300

    lot changes

--- src/main/java/com/thorinhood/dataworker/configs/VKConfiguration.java
+++ src/main/java/com/thorinhood/dataworker/configs/VKConfiguration.java
@@ -30,7 +30,7 @@ public class VKConfiguration {
 
     @Bean
     public VKFriendsService vkFriendsService() {
-        return new VKFriendsService(30);
+        return new VKFriendsService(20);
     }
 
 }
